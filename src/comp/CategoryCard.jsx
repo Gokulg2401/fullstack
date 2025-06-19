3e2d7e18859bb244c89fbabcaba558338470d254
Gokulg2401 <gokul240102@gmail.com>
2025-06-19 21:00:02 +0530

    Remerge

--- src/comp/CategoryCard.jsx
+++ src/comp/CategoryCard.jsx
@@ -1,41 +1,37 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { showInfo } from './Toast';
+import women from '../comp/icon/women.webp';
+import kids from '../comp/icon/kids.webp';
 
-import { showInfo } from '../comp/Toast';
-
-const CategoryCard = ({ title, img,onClick }) => {
+const CategoryCard = ({ title, img }) => {
   const navigate = useNavigate();
-  
+
   const handleClick = () => {
     showInfo(`${title} category selected`);
-    if (onClick) onClick(); // call parent function to change category
-    const category = title.toLowerCase(); // men, women, kids
+    const category = title.toLowerCase();
     navigate(`/category/${category}`);
   };
-  // 🧠 Helper to return emoji based on title
-  const getEmoji = (title) => {
-  const cleanTitle = title.trim().toLowerCase(); // Normalize
 
-  switch (cleanTitle) {
-    case 'men':
-      return '👔';
-    case 'women':
-      return '🥻';
-    case 'kids':
-      return '👶🏼';
-    default:
-      return '🛍️';
-  }
-};
+  const getImage = () => {
+    const lowerTitle = title.toLowerCase();
+    if (lowerTitle === 'women') return women;
+    if (lowerTitle === 'kids') return kids;
+    return img;
+  };
 
+  const getTitleWithEmoji = () => {
+    if (title.toLowerCase() === 'Men') return '🧥 Men';
+    if (title.toLowerCase() === 'Women') return '👗 Women';
+    if (title.toLowerCase() === 'Kids') return '🧒 Kids';
+    return title;
+  };
 
   return (
     <div className="category-card" onClick={handleClick}>
-      
-      <img src={img} alt={title} className="category-img" />
-      <h3 className='card-title'>{getEmoji(title)} {title}</h3>
+      <img src={getImage()} alt={title} className="category-img" />
+      <h3>{getTitleWithEmoji()}</h3>
     </div>
-    
   );
 };
 
