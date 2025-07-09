977046cca92f239bd390bc02be2d50f150cf9abe
Gokulg2401 <gokul240102@gmail.com>
2025-07-09 17:00:11 +0530

    gkl

---
+++ demo/src/main/java/com/gklproject/demo/GreetingConfig.java
@@ -0,0 +1,6 @@
+package com.gklproject.demo;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+public class GreetingConfig {
+    
+}
---
+++ demo/src/main/java/com/gklproject/demo/GreetingService.java
@@ -0,0 +1,6 @@
+package com.gklproject.demo;
+public class GreetingService {
+    public String getMessage(){
+        return "Hello from Gkl";
+    }
+}
