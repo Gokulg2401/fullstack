c11d1bef26ef4f655026ee17cd59b4f24f262f24
Gokulg2401 <gokul240102@gmail.com>
2025-07-14 12:50:07 +0530

    c

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,34 +1,22 @@
-import React, { useState } from 'react';
+import React,{useState,useEffect} from "react";
+const Todo=()=>{
+  const[todo,setTodo]=useState(null);
+  useEffect(()=>{
+    fetchTodo=async()=>{
+      const res=await fetch("https://dummyjson.com/todos");
+      const data=await res.json();
+      setTodo(data);
+    }
+    fetchTodo();
 
-function Todo() {
-  const [input, setInput] = useState('');
-  const [todos, setTodos] = useState([]);
-
-  const handleAdd = () => {
-    if (input.trim() === '') return;
-
-    setTodos([...todos, input]); // Add new item to the array
-    setInput(''); // Clear input field
-  };
-
-  return (
-    <div class='todo'>
-      <h2>To-Do List</h2>
-      <input 
-        type="text" 
-        value={input} 
-        onChange={(e) => setInput(e.target.value)} 
-        placeholder="Enter a task" 
-      />
-      <button onClick={handleAdd}style={{backgroundColor:'black',color:'red'}}>Add</button>
-
-      <ul>
-        {todos.map((item, index) => (
-          <li key={index}>{item}</li> // Render each item
-        ))}
-      </ul>
+  });
+  return(
+    <div>
+      <p>ID:{todo.id}</p>
+      <p>TODO:{todo.todo}</p>
+      <p>Status:{todo.completed}</p>
+      <p>USerId:{todo.userID}</p>
     </div>
   );
 }
-
 export default Todo;
