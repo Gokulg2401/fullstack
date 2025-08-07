14e4cb2bfc8d33e348514b27370115fc843fb996
Gokulg2401 <gokul240102@gmail.com>
2025-08-07 22:59:31 +0530

    gdg

--- demo/src/main/java/com/gklproject/demo/config/CorsConfig.java
+++ demo/src/main/java/com/gklproject/demo/config/CorsConfig.java
@@ -1,11 +1,9 @@
 package com.gklproject.demo.config;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import io.micrometer.common.lang.NonNull;
 
 @Configuration
 public class CorsConfig {
@@ -13,10 +11,9 @@ public class CorsConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
-            @NonNull
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**") // Adjust as needed
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/api/**")
                         .allowedOrigins("http://localhost:3000", "https://your-frontend-domain.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowCredentials(true);
--- demo/src/main/java/com/gklproject/demo/config/WebConfig.java
+++ demo/src/main/java/com/gklproject/demo/config/WebConfig.java
@@ -1,16 +1,14 @@
 package com.gklproject.demo.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import io.micrometer.common.lang.NonNull;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
-    @NonNull
-    public void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/api/**")
                 .allowedOrigins("http://localhost:3000")
                 .allowedMethods("*")
